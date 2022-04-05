794631eba7416ccaeff6747c0869c2f92dd80003
hanshino <c0952199517@gmail.com>
2022-04-05 12:18:47 +0800

    add i18n configuration

---
+++ src/i18n.jsx
@@ -0,0 +1,31 @@
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: "/static/locales/{{lng}}/{{ns}}.json",
+    },
+    fallbackLng: "zh-TW",
+    supportedLngs: ["zh-TW", "en"],
+    debug: false,
+    detection: {
+      order: ["querystring", "localStorage"],
+      caches: ["localStorage"],
+      lookupQuerystring: "lang",
+      lookupLocalStorage: "Lang",
+    },
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    react: {
+      useSuspense: true,
+    },
+  });
+
+export default i18n;
